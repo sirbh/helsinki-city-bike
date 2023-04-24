6226a6e5f520c2154ff50ad2e3a209773ae5708d
saurabh chauhan <sourabh1202@gmail.com>
2023-04-25 00:47:24 +0300

    fixed frontend lint issues #skip deploy

--- front-end/src/App.test.tsx
+++ front-end/src/App.test.tsx
@@ -1,11 +1,11 @@
 import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-    it('should render Hello World', () => {
-        const { container } = render(<App />);
+  it('should render Hello World', () => {
+    const { container } = render(<App />);
 
-        expect(container.querySelector('#app')).toHaveTextContent('Hello');
-    });
+    expect(container.querySelector('#app')).toHaveTextContent('Hello');
+  });
 });
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,18 +3,18 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-    const [info, setInfo] = useState();
-    useEffect(() => {
-        axios.get('/api/info').then((resp) => {
-            setInfo(resp.data);
-        });
-    }, []);
-    return (
-        <div id="app">
-            <h1>Hello</h1>
-            <h1>{info}</h1>
-        </div>
-    );
+  const [info, setInfo] = useState();
+  useEffect(() => {
+    axios.get('/api/info').then((resp) => {
+      setInfo(resp.data);
+    });
+  }, []);
+  return (
+    <div id="app">
+      <h1>Hello</h1>
+      <h1>{info}</h1>
+    </div>
+  );
 }
 
 export default App;
