d682d9dfc75e0ad4761d9592ddcda6f994f30912
saurabh chauhan <sourabh1202@gmail.com>
2023-04-25 00:57:11 +0300

    fixed test #skip deploy

--- test/ping.test.ts
+++ test/ping.test.ts
@@ -5,7 +5,6 @@ import app from '../app';
 const api = supertest(app);
 
 test('if /api/info respond with 200',async ()=>{
-  await api.get('/ping')
-           .expect(200)
-           .expect(`helsinki city bike api ${new Date()}`);
+  await api.get('/api/info')
+           .expect(200);
 });
