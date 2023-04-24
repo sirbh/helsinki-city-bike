f3f2c87254c5a80ddfe29c1190bdc67071625e63
saurabh chauhan <sourabh1202@gmail.com>
2023-04-24 20:02:09 +0300

    added testing

---
+++ front-end/src/App.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+    it('should render Hello World', () => {
+        const { container } = render(<App />);
+
+        expect(container.querySelector('#app')).toHaveTextContent(
+            'Hello World'
+        );
+    });
+});
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-    return <div>Hello World</div>;
+    return <div id="app">Hello World</div>;
 }
 
 export default App;
