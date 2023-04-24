d7eb8c134a08c868bc64954d02567a6c3b13f6f0
saurabh chauhan <sourabh1202@gmail.com>
2023-04-25 01:01:30 +0300

    fixed frontend test #skip deploy

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,18 +1,9 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import axios from 'axios';
 
 function App() {
-  const [info, setInfo] = useState();
-  useEffect(() => {
-    axios.get('/api/info').then((resp) => {
-      setInfo(resp.data);
-    });
-  }, []);
   return (
     <div id="app">
       <h1>Hello</h1>
-      <h1>{info}</h1>
     </div>
   );
 }
