7d79d4d2f861e0767a830657d0f385faf99118f6
saurabh chauhan <sourabh1202@gmail.com>
2023-05-01 22:37:20 +0300

    fixed useQuery issue

--- front-end/src/hooks/useJourneyDetails.ts
+++ front-end/src/hooks/useJourneyDetails.ts
@@ -5,9 +5,14 @@ import getJourneyDetails from '../services';
 const useJourneyDetails = () => {
   const [page, setPage] = useState<number>(1);
   const take = 10; // number of item to show per page
-  const { data, error, isLoading } = useQuery(['journeys', page, take], () =>
-    getJourneyDetails(page, 10)
-  );
+  // const { data, error, isLoading } = useQuery(['journeys',page], () =>
+  //   getJourneyDetails(page, 10)
+  // );
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['journeys', page],
+    queryFn: () => getJourneyDetails(page, take),
+  });
 
   return { page, setPage, data, error, isLoading, take };
 };
