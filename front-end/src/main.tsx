59979022a29088896b74c81929c618c7cd4bbde3
saurabh chauhan <sourabh1202@gmail.com>
2023-05-01 15:04:55 +0300

    added router config

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <RouterProvider router={router} />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
