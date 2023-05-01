4e460a70f09323d79a143d7858bd612aa9889ee9
saurabh chauhan <sourabh1202@gmail.com>
2023-05-01 21:26:52 +0300

    added router config

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import Journeys from '../components/journeys';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: '/',
+        element: <Journeys />,
+      },
+    ],
   },
 ]);
 export default router;
