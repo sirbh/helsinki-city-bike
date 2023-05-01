4fd74088325144b65b4fda58539181e0f1be9f58
saurabh chauhan <sourabh1202@gmail.com>
2023-05-01 21:20:54 +0300

    updated journey api to send total records

--- controllers/journeys/index.ts
+++ controllers/journeys/index.ts
@@ -1,49 +1,55 @@
-import { Router } from "express";
+import { RequestHandler, Router } from "express";
 import { db } from "../../prisma";
 import { ValidateJourneyRequest } from "../../middlewares";
 import { jourenyRequestValidators } from "../../util/validators";
 
 const journeyRouter = Router();
 
-journeyRouter.get("/", ValidateJourneyRequest, (req, res, next) => {
-
-  const { take, page, sortBy,filterBy } = jourenyRequestValidators.cast({
-    take:req.query.take,
-    page:req.query.page,
-    sortBy:{
-        property:req.query.sort_prop,
-        order:req.query.order
+journeyRouter.get("/", ValidateJourneyRequest, (async (req, res, next) => {
+  const { take, page, sortBy, filterBy } = jourenyRequestValidators.cast({
+    take: req.query.take,
+    page: req.query.page,
+    sortBy: {
+      property: req.query.sort_prop,
+      order: req.query.order,
     },
     filterBy: req.query.id
-    ? {
-        property: req.query.filter_prop,
-        id: req.query.id,
-      }
-    : undefined,
+      ? {
+          property: req.query.filter_prop,
+          id: req.query.id,
+        }
+      : undefined,
   });
 
-  const _sortBy:{[key:string]:string} = {};
-  const _where:{[key:string]:number}= {};
+  const _sortBy: { [key: string]: string } = {};
+  const _where: { [key: string]: number } = {};
 
   _sortBy[sortBy.property] = sortBy.order;
 
-  if(filterBy){
-    _where[filterBy.property]=filterBy.id;
+  if (filterBy) {
+    _where[filterBy.property] = filterBy.id;
   }
 
-  db.journeys
-    .findMany({
-      take: take,
-      skip: take * (page - 1),
-      orderBy: _sortBy,
-      where:_where
-    })
-    .then((resp) => {
-      return res.status(200).send(resp);
-    })
-    .catch((e) => {
-      next(e);
+  try {
+    const journeyDetails = await db.$transaction([
+      db.journeys.findMany({
+        take: take,
+        skip: take * (page - 1),
+        orderBy: _sortBy,
+        where: _where,
+      }),
+      db.journeys.count({
+        where: _where,
+      }),
+    ]);
+
+    res.status(200).send({
+      count: journeyDetails[1],
+      details: journeyDetails[0],
     });
-});
+  } catch (e) {
+    next(e);
+  }
+}) as RequestHandler);
 
 export default journeyRouter;
