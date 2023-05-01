23ca99c4e1937abf4808c55adc568c6dba68face
saurabh chauhan <sourabh1202@gmail.com>
2023-05-01 21:25:49 +0300

    added query client

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,14 @@
 import Layout from './components/layout';
 import './App.css';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 function App() {
-  return <Layout />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
