28266d49b4ecccc1da2ed69a087ff9dc1400b043
saurabh chauhan <sourabh1202@gmail.com>
2023-05-02 22:05:01 +0300

    added tabs to jouneys page

--- front-end/src/components/journeys/index.tsx
+++ front-end/src/components/journeys/index.tsx
@@ -1,24 +1,31 @@
+import { LinearProgress } from '@mui/material';
 import JourneyTable from './table';
 import useJourneyDetails from '../../hooks/useJourneyDetails';
+import Tabs from './tabs';
 
 function Journeys() {
-  const { data, error, isLoading, page, setPage, take } = useJourneyDetails();
+  const { data, error, isLoading, page, take, setOrder, setSortBy, setPage } =
+    useJourneyDetails();
   if (error) {
     return <div>Error</div>;
   }
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   return (
-    <JourneyTable
-      count={data?.count ? data.count : 0}
-      page={page}
-      pageChangeHandler={(p) => {
-        setPage(p);
-      }}
-      tableData={data?.details ? data.details : []}
-      take={take}
-    />
+    <>
+      <Tabs setOrder={setOrder} setSortBy={setSortBy} />
+      {isLoading ? (
+        <LinearProgress />
+      ) : (
+        <JourneyTable
+          count={data?.count ? data.count : 0}
+          page={page}
+          pageChangeHandler={(p) => {
+            setPage(p);
+          }}
+          tableData={data?.details ? data.details : []}
+          take={take}
+        />
+      )}
+    </>
   );
 }
 
