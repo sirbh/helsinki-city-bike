a85c5382f9bcf023ddcfac381687aa4771f711ba
saurabh chauhan <sourabh1202@gmail.com>
2023-05-02 18:20:03 +0300

    added journey table e2e test #skipdeploy

---
+++ cypress/e2e/journeyTableTest.cy.ts
@@ -0,0 +1,20 @@
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+  it("should have correct header", () => {
+    cy.get("table thead tr th:nth-child(1)").should("contain", 'Departure station');
+    cy.get("table thead tr th:nth-child(2)").should("contain", 'Return Station');
+    cy.get("table thead tr th:nth-child(3)").should("contain", 'Covered Distance');
+    cy.get("table thead tr th:nth-child(4)").should("contain", 'Duration');
+  });
+  it("should have correct number of rows", () => {
+    cy.get("table tbody tr").should("have.length", 10);
+  });
+  it('should have the correct content in a specific cell', () => {
+    cy.get('table tbody tr:nth-child(2) td:nth-child(1)').should('contain', 'Koskelan varikko');
+    cy.get('table tbody tr:nth-child(2) td:nth-child(2)').should('contain', 'Paavalinpuisto');
+    cy.get('table tbody tr:nth-child(2) td:nth-child(3)').should('contain', 1.71);
+    cy.get('table tbody tr:nth-child(2) td:nth-child(4)').should('contain', 6);
+  });
+});
