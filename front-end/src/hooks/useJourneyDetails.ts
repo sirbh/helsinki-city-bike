e710c825a67df3bf8317960ad7bcfd93fbb5247c
saurabh chauhan <sourabh1202@gmail.com>
2023-05-03 09:47:37 +0300

    updated joureny detail hook to allow filtering

--- front-end/src/hooks/useJourneyDetails.ts
+++ front-end/src/hooks/useJourneyDetails.ts
@@ -1,19 +1,36 @@
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import getJourneyDetails from '../services';
+import { getJourneyDetails } from '../services';
 
 const useJourneyDetails = () => {
   const [page, setPage] = useState<number>(1);
   const take = 10; // number of item to show per page
   const [order, setOrder] = useState<string>('asc');
   const [sortBy, setSortBy] = useState<string>('departure_station_name');
+  const [stationId, setStationId] = useState<string>('');
+  const [journeyType, setJourneyType] = useState<string>(
+    'departure_station_id'
+  );
 
   const { data, error, isLoading } = useQuery({
-    queryKey: ['journeys', page, order, sortBy],
-    queryFn: () => getJourneyDetails(page, take, sortBy, order),
+    queryKey: ['journeys', page, order, sortBy, stationId, journeyType],
+    queryFn: () =>
+      getJourneyDetails(page, take, sortBy, order, stationId, journeyType),
   });
 
-  return { page, setPage, data, error, isLoading, take, setOrder, setSortBy };
+  return {
+    page,
+    data,
+    error,
+    isLoading,
+    take,
+    journeyType,
+    setOrder,
+    setSortBy,
+    setPage,
+    setStationId,
+    setJourneyType,
+  };
 };
 
 export default useJourneyDetails;
