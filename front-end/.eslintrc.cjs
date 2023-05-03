fae7843c2dd011932dad51faa143cf1936050d7b
saurabh chauhan <sourabh1202@gmail.com>
2023-05-03 09:46:20 +0300

    allow props spreading in front end

--- front-end/.eslintrc.cjs
+++ front-end/.eslintrc.cjs
@@ -1,27 +1,28 @@
 module.exports = {
-    // "root":true,
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'airbnb',
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    root: true,
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json',
-    },
-    plugins: ['react', '@typescript-eslint', 'prettier'],
-    rules: {
-        'react/react-in-jsx-scope': 0,
-    },
+  // "root":true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 'off',
+  },
 };
