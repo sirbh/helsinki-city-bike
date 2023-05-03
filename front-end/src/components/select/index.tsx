5f0d436507821f7fd6bab7494943556d5daf49cf
saurabh chauhan <sourabh1202@gmail.com>
2023-05-03 09:48:47 +0300

    added select

---
+++ front-end/src/components/select/index.tsx
@@ -0,0 +1,43 @@
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { SelectOption } from '../../types';
+
+interface SelectInputProps {
+  options: SelectOption[];
+  selectedJourneyType: string;
+  handleChange: (e: string) => void;
+}
+
+function SelectInput({
+  options,
+  selectedJourneyType,
+  handleChange,
+}: SelectInputProps) {
+  return (
+    <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <InputLabel id="demo-simple-select-autowidth-label">
+        Journey Type
+      </InputLabel>
+      <Select
+        labelId="demo-simple-select-autowidth-label"
+        id="demo-simple-select-autowidth"
+        value={selectedJourneyType}
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        autoWidth
+        label="Journey Type"
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default SelectInput;
