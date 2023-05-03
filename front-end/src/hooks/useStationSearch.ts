137cfa849162f964369a289a35616fe0a21b1957
saurabh chauhan <sourabh1202@gmail.com>
2023-05-03 09:47:04 +0300

    added search station hook

---
+++ front-end/src/hooks/useStationSearch.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { searchStation } from '../services';
+
+const useStationSearch = () => {
+  const [query, setQuery] = useState<string>('');
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['journeys', query],
+    queryFn: () => searchStation(query),
+    enabled: query !== '',
+  });
+
+  return { setQuery, data, error, isLoading };
+};
+
+export default useStationSearch;
