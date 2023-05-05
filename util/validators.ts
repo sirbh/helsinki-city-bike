f33e6b5eff8f54c362bcf82d5c3e1093dd11eb3b
saurabh chauhan <sourabh1202@gmail.com>
2023-05-05 20:56:45 +0300

    added add station validators

--- util/validators.ts
+++ util/validators.ts
@@ -32,6 +32,16 @@ export const jourenyRequestValidators = object({
   }).default(undefined),
 });
 
+export const addStationValidators = object({
+  name:string().required('name is required is field').test('len', 'should be more than 3 characters', (val) => val.length > 3),
+  address:string().required('address is required field').test('len', 'should be more than 3 characters', (val) => val.length > 3),
+  city:string().required('city is required field').test('len', 'should be more than 3 characters', (val) => val.length > 3),
+  operator:string().required('operator is required field').test('len', 'should be more than 3 characters', (val) => val.length > 3),
+  capacity:number().integer().min(1,"should more than 0").required('capacity is required field'),
+  x:number().required('lat is required'),
+  y:number().required('long is required')
+});
+
 export const stationRequestValidators = object({
   take: number().required("property take is required").positive().integer(),
   page: number().required("property page is required").integer().positive(),
