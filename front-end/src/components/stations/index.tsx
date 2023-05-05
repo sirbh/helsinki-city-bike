ae8e010e0b0574aa10fe471dbbe33266e2a24c29
saurabh chauhan <sourabh1202@gmail.com>
2023-05-06 00:03:36 +0300

    removed unused eslint comment

--- front-end/src/components/stations/index.tsx
+++ front-end/src/components/stations/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Box, LinearProgress } from '@mui/material';
 import { useState } from 'react';
 import useStationDetails from '../../hooks/useStationDetails';
