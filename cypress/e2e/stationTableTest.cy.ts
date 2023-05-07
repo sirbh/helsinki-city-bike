84603910c898b04611ec44eeac2c8e4c80c83cfd
saurabh chauhan <sourabh1202@gmail.com>
2023-05-07 20:05:49 +0300

    updated modal test

--- cypress/e2e/stationTableTest.cy.ts
+++ cypress/e2e/stationTableTest.cy.ts
@@ -1,4 +1,3 @@
-
 describe("stations table tests", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
@@ -30,35 +29,42 @@ describe("stations table tests", () => {
     );
     cy.get("table tbody tr:nth-child(2) td:nth-child(3)").should(
       "contain",
-     "Gräsviksgatan 23"
+      "Gräsviksgatan 23"
     );
-    cy.get("table tbody tr:nth-child(2) td:nth-child(4)").should(
-      "contain",
-      16
-    );
+    cy.get("table tbody tr:nth-child(2) td:nth-child(4)").should("contain", 16);
   });
 
-  it("should be able to change the page when clicked on the arrows",()=>{
-    cy.get('[class^=MuiTablePagination]').contains('1–10 of 11');
-    cy.get('[data-testid=KeyboardArrowRightIcon]').click();
+  it("should be able to change the page when clicked on the arrows", () => {
+    cy.get("[class^=MuiTablePagination]").contains("1–10 of 11");
+    cy.get("[data-testid=KeyboardArrowRightIcon]").click();
     cy.wait(500);
-    cy.get('[class^=MuiTablePagination]').contains('11–11 of 11');
-    cy.get('[data-testid=KeyboardArrowLeftIcon]').click();
+    cy.get("[class^=MuiTablePagination]").contains("11–11 of 11");
+    cy.get("[data-testid=KeyboardArrowLeftIcon]").click();
     cy.wait(500);
-    cy.get('[class^=MuiTablePagination]').contains('1–10 of 11');
-});
+    cy.get("[class^=MuiTablePagination]").contains("1–10 of 11");
+  });
 
- it("should show modal when clicked on the station" , ()=>{
-    cy.get("table tbody tr:nth-child(2) td:nth-child(1)").contains('Baana').click();
+  it("should show modal when clicked on the station", () => {
+    cy.get("table tbody tr:nth-child(2) td:nth-child(1)")
+      .contains("Baana")
+      .click();
     cy.wait(500);
-    cy.get('[class^=MuiModal-root]').find('div').should('have.class','MuiPaper-root');
- });
+    cy.get("[class^=MuiModal-root]")
+      .find("div")
+      .should("have.class", "MuiPaper-root");
+  });
 
- it("should show modal when clicked on the station shown in autocomplete dropdown" , ()=>{
-    cy.get('input').first().type('aa');
+  it("should show modal when clicked on the station shown in autocomplete dropdown", () => {
+    cy.get("input").first().type("aa");
     cy.wait(500);
-    cy.get('[class^=MuiAutocomplete-popper]').contains('Aalto University (M), Korkeakoulua').click();
+    cy.get("[class^=MuiAutocomplete-popper]")
+      .contains("Aalto University (M), Korkeakoulua")
+      .click();
     cy.wait(500);
-    cy.get('[class^=MuiModal-root]').find('div').should('have.class','MuiPaper-root');
- });
+    cy.get("[class^=MuiModal-root]")
+      .find("div")
+      .should("have.class", "MuiPaper-root")
+      .find("div")
+      .should("have.class", "MuiCardHeader-content");
+  });
 });
