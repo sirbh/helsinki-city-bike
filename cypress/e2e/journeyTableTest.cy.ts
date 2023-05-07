b70fbfbac7579b6dceff7c4ee4b366d28fddf56c
saurabh chauhan <sourabh1202@gmail.com>
2023-05-07 16:08:35 +0300

    added tabs e2e tests

--- cypress/e2e/journeyTableTest.cy.ts
+++ cypress/e2e/journeyTableTest.cy.ts
@@ -1,4 +1,9 @@
-describe("template spec", () => {
+// import { journeys } from "../../util/helperdata";
+
+// import { journeys } from "../../util/helperdata";
+import { getStationSortedByDeparture, getStationSortedByDistance, getStationSortedByDuration, getStationSortedByReturn } from "../../util/testHelper";
+
+describe("journey table test", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
@@ -38,4 +43,147 @@ describe("template spec", () => {
       2.5
     );
   });
+
+  it("should be sorted by departure station by default", () => {
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const departureStations = cellDataArray.filter((_d, i) => i % 4 === 0);
+        expect(departureStations).to.deep.equal(
+          getStationSortedByDeparture("asc")
+            .map((station) => station.departure_station_name)
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by departure station if clicked on the departure station tab",() => {
+    cy.contains('Departure Station').click();
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const departureStations = cellDataArray.filter((_d, i) => i % 4 === 0);
+        expect(departureStations).to.deep.equal(
+          getStationSortedByDeparture("desc")
+            .map((station) => station.departure_station_name)
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by return station if clicked on the return station tab",() => {
+    cy.contains('Return Station').click();
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const returnStations = cellDataArray.filter((_d, i) => i % 4 === 1);
+        expect(returnStations).to.deep.equal(
+          getStationSortedByReturn("asc")
+            .map((station) => station.return_station_name)
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by return station in descending order if clicked twice on the return station tab",() => {
+    cy.contains('Return Station').click();
+    cy.wait(1000);
+    cy.contains('Return Station').click();
+    cy.wait(1000);
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const returnStations = cellDataArray.filter((_d, i) => i % 4 === 1);
+        expect(returnStations).to.deep.equal(
+          getStationSortedByReturn("desc")
+            .map((station) => station.return_station_name)
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by covered distance in ascending order if clicked on the covered distance tab",() => {
+    cy.contains('Covered Distance').click();
+    cy.wait(1000);
+
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const distanceArray = cellDataArray.filter((_d, i) => i % 4 === 2);
+        expect(distanceArray).to.deep.equal(
+          getStationSortedByDistance("asc")
+            .map((station) => (station.covered_distance/1000).toFixed(2))
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by covered distance in descending order if clicked on the covered distance tab twice",() => {
+    cy.contains('Covered Distance').click();
+    cy.wait(1000);
+    cy.contains('Covered Distance').click();
+    cy.wait(1000);
+
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const distanceArray = cellDataArray.filter((_d, i) => i % 4 === 2);
+        expect(distanceArray).to.deep.equal(
+          getStationSortedByDistance("desc")
+            .map((station) => (station.covered_distance/1000).toFixed(2))
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by duration in ascending order if clicked on the duration tab",() => {
+    cy.contains('Duration').click();
+    cy.wait(1000);
+
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const durationArray = cellDataArray.filter((_d, i) => i % 4 === 3);
+        expect(durationArray).to.deep.equal(
+          getStationSortedByDuration("asc")
+            .map((station) => (station.duration/60).toFixed(1))
+            .slice(0, 10)
+        );
+      });
+  });
+
+  it("should be sorted by duration in desceding order if clicked on the duration tab twice",() => {
+    cy.contains('Duration').click();
+    cy.wait(1000);
+    cy.contains('Duration').click();
+    cy.wait(1000);
+
+    cy.get("table tbody tr td")
+      .then(($el) => {
+        return Cypress._.map($el, "innerText");
+      })
+      .then((cellDataArray) => {
+        const durationArray = cellDataArray.filter((_d, i) => i % 4 === 3);
+        expect(durationArray).to.deep.equal(
+          getStationSortedByDuration("desc")
+            .map((station) => (station.duration/60).toFixed(1))
+            .slice(0, 10)
+        );
+      });
+  });
+
 });
--- cypress/e2e/layouttest.cy.ts
+++ cypress/e2e/layouttest.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('layout should have all the routes', () => {
+  it('should contain jourenys and station navigators', () => {
     cy.visit('http://localhost:3000');
     cy.contains('citybike',{matchCase:false});
     cy.contains('stations',{matchCase:false});
