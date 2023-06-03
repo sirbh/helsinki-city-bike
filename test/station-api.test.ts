7eb0a81bb19ae5dc1fd5d8296e1b0d2694a52084
saurabh chauhan <sourabh1202@gmail.com>
2023-06-03 23:10:40 +0300

    updated test to filter station based on user

--- test/station-api.test.ts
+++ test/station-api.test.ts
@@ -1,6 +1,6 @@
 import supertest from "supertest";
 import app from "../app";
-import { afterAll, expect, test } from "@jest/globals";
+import { afterAll, beforeAll, expect, test } from "@jest/globals";
 import { stations as StationsSchema } from "@prisma/client";
 import { stations as stationsHelperData } from "../util/helperdata";
 import { db } from "../prisma";
@@ -56,8 +56,53 @@ test('if search stations api return correct result according to "query"', async 
 
   journeys_resp2.forEach(journey=>{
     expect(journey.name.slice(0,2).toLowerCase()).toEqual('ka');
-  });
+  }); 
+});
+
+describe("stations api should return station added by user if username is provided in request",()=>{
+  let token = "";
+  beforeAll(async ()=>{
+    const response = await api.post("/api/auth/login").send({
+      username: "tempusername",
+      password: "password!2A",
+    });
+    const details = response.body as { token: string };
+    token = details.token;
+    await api
+    .post("/api/stations")
+    .set("Authorization", `Bearer ${token}`)
+    .send({
+      name: "test name",
+      address: "test address",
+      city: "test city",
+      operator: "test operator",
+      capacity: 4,
+      x: 12,
+      y: 4,
+    });
+  },10000);
+
   
+
+  afterAll(async ()=>{
+    await db.stations.deleteMany({
+      where:{
+        name:'test name'
+      }
+    });
+  });
+
+  test('if get stations api return station added by authenticated user if username is given', async () => {
+    const response = await api.get("/api/stations?page=1&take=5&username=tempusername");
+    expect(response.body.details).toHaveLength(1);
+    expect(response.body.count).toBe(1);
+    const journeys_resp = response.body.details as StationsSchema[];
+    journeys_resp.forEach((journey) => {
+      expect(journey.name).toEqual('test name');
+      expect(journey.address).toEqual('test address');
+      expect(journey.city).toEqual('test city');
+    });
+  },10000);
 });
 
 
