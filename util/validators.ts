d1c68636d6a43074c81f4b7b499c626c79b0446f
saurabh chauhan <sourabh1202@gmail.com>
2023-06-03 23:09:22 +0300

    fixed formatting and added username key

--- util/validators.ts
+++ util/validators.ts
@@ -35,27 +35,45 @@ export const jourenyRequestValidators = object({
 export const addStationValidators = object({
   name: string()
     .required("name is required field")
-    .test("len", "name should be more than 3 characters", (val) => val.length >= 3),
+    .test(
+      "len",
+      "name should be more than 3 characters",
+      (val) => val.length >= 3
+    ),
   address: string()
     .required("address is required field")
-    .test("len", "address should be more than 3 characters", (val) => val.length >= 3),
+    .test(
+      "len",
+      "address should be more than 3 characters",
+      (val) => val.length >= 3
+    ),
   city: string()
     .required("city is required field")
-    .test("len", "city should be more than 3 characters", (val) => val.length >= 3),
+    .test(
+      "len",
+      "city should be more than 3 characters",
+      (val) => val.length >= 3
+    ),
   operator: string()
     .required("operator is required field")
-    .test("len", "operator should be more than 3 characters", (val) => val.length >= 3),
-  capacity: number().typeError('capacity should be a number')
-    .integer('capacity should be a integer')
+    .test(
+      "len",
+      "operator should be more than 3 characters",
+      (val) => val.length >= 3
+    ),
+  capacity: number()
+    .typeError("capacity should be a number")
+    .integer("capacity should be a integer")
     .min(1, "capacity should be more than 0")
     .required("capacity is required field"),
-  x: number().typeError('lat should be a number').required("lat is required"),
-  y: number().typeError('long should be a number').required("long is required"),
+  x: number().typeError("lat should be a number").required("lat is required"),
+  y: number().typeError("long should be a number").required("long is required"),
 });
 
 export const stationRequestValidators = object({
   take: number().required("property take is required").positive().integer(),
   page: number().required("property page is required").integer().positive(),
+  username: string().optional(),
 });
 
 export const stationsSearchValidator = object({
@@ -68,7 +86,11 @@ export const userValidator = object({
     .test("len", "name must be more than 2 chars", (val) => val.length >= 3),
   username: string()
     .required()
-    .test("len", "username must be more than 2 chars", (val) => val.length >= 3),
+    .test(
+      "len",
+      "username must be more than 2 chars",
+      (val) => val.length >= 3
+    ),
   password: string()
     .required("Please Enter your password")
     .matches(
@@ -78,6 +100,6 @@ export const userValidator = object({
 });
 
 export const loginDetailsValidators = object({
-  username: string().required('username is required'),
-  password:string().required('password is required')
+  username: string().required("username is required"),
+  password: string().required("password is required"),
 });
