0673cdbeff6316363d8813f200d3f82919927e34
saurabh chauhan <sourabh1202@gmail.com>
2023-06-04 00:26:06 +0300

    added e2e testing for station filter

--- cypress/e2e/stationTableTest.cy.ts
+++ cypress/e2e/stationTableTest.cy.ts
@@ -35,13 +35,13 @@ describe("stations table tests", () => {
   });
 
   it("should be able to change the page when clicked on the arrows", () => {
-    cy.get("[class^=MuiTablePagination]").contains("1–10 of 11");
+    cy.get("p.MuiTablePagination-displayedRows").contains("1–10 of 11");
     cy.get("[data-testid=KeyboardArrowRightIcon]").click();
     cy.wait(500);
-    cy.get("[class^=MuiTablePagination]").contains("11–11 of 11");
+    cy.get("p.MuiTablePagination-displayedRows").contains("11–11 of 11");
     cy.get("[data-testid=KeyboardArrowLeftIcon]").click();
     cy.wait(500);
-    cy.get("[class^=MuiTablePagination]").contains("1–10 of 11");
+    cy.get("p.MuiTablePagination-displayedRows").contains("1–10 of 11");
   });
 
   it("should show modal when clicked on the station", () => {
@@ -66,5 +66,60 @@ describe("stations table tests", () => {
       .should("have.class", "MuiPaper-root")
       .find("div")
       .should("have.class", "MuiCardHeader-content");
+  });
+
+  it("should not show checkbox if user is not logged in",()=>{
+    cy.get("span.MuiCheckbox-root").should('not.exist');
+  });
+});
+
+
+describe("station table test that require authentication", () => {
+  before(() => {
+    cy.visit("");
+    cy.clearLocalStorage();
+    cy.contains("login", { matchCase: false }).click();
+    cy.get("div.MuiCardContent-root").within(() => {
+      cy.get('input[name="username"]').type("tempusername");
+      cy.get('input[name="password"]').type("password!2A");
+      cy.contains("login").click();
+    });
+    cy.get("div.MuiAvatar-root").click();
+    cy.get("div.MuiMenu-paper").within(() => {
+      cy.contains("Add another station").click();
+    });
+    cy.get("div.MuiCardContent-root").within(() => {
+      cy.get('input[name="name"]').type("some place");
+      cy.get('input[name="address"]').type("45 some address");
+      cy.get('input[name="longitude"]').type("-27");
+      cy.get('input[name="latitude"]').type("-89");
+      cy.get('input[name="capacity"]').type("4");
+
+      cy.contains("Submit").click();
+    });
+    cy.wait(4000);
+    cy.get("body").click(0, 0);
+  });
+
+
+  after(()=>{
+    cy.get('input[id="combo-box-demo"]').type('some place');
+    cy.wait(4000);
+    cy.get("[class^=MuiAutocomplete-popper]").contains(
+        "some place"
+      ).click();
+    cy.wait(4000);
+    cy.contains('delete station').click();
+    cy.get('body').click(0,0);
+  });
+
+  it("should contain checkbox to filter station based on user", () => {
+    cy.contains("stations", { matchCase: false }).click();
+    cy.get("span.MuiCheckbox-root")
+      .siblings()
+      .first()
+      .contains("Show Stations Added By You");
+    cy.get("span.MuiCheckbox-root").click();
+    cy.get("table tbody tr").should("have.length", 1);
   });
 });
