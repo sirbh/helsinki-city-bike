bd6ed030453804d1669430b6ce6fb0064ad9dc77
saurabh chauhan <sourabh1202@gmail.com>
2023-06-27 19:53:29 +0300

    fixed lint issues

--- front-end/src/services/journeys/index.ts
+++ front-end/src/services/journeys/index.ts
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import { IJourneyAPIResponse } from '../../types';
 
+const getJourneyDetails = async (
+  page: number,
+  take: number,
+  sortBy: string,
+  order: string,
+  stationId: string,
+  journeyType: string
+) => {
+  const { data } = await axios.get<IJourneyAPIResponse>(
+    `/api/journeys?page=${page}&take=${take}&sort_prop=${sortBy}&order=${order}&filter_prop=${journeyType}&id=${stationId}`
+  );
 
+  return data;
+};
 
-export const getJourneyDetails = async (
-    page: number,
-    take: number,
-    sortBy: string,
-    order: string,
-    stationId: string,
-    journeyType: string
-  ) => {
-    const { data } = await axios.get<IJourneyAPIResponse>(
-      `/api/journeys?page=${page}&take=${take}&sort_prop=${sortBy}&order=${order}&filter_prop=${journeyType}&id=${stationId}`
-    );
-  
-    return data;
-  };
+export default getJourneyDetails;
